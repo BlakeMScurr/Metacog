b9acac24a7d246dc29c8dd09575f0b94a02aab5c
blakemscurr <blakemscurr@gmail.com>
2021-06-08 14:12:27 +1200

    Move up and implement initial selector thingy.

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Selector from "../components/Selector.svelte";
+
+    function onSelect(event) {
+        console.log(event.detail.choices)
+    }
+</script>
+
+<Selector on:select={onSelect}></Selector>
