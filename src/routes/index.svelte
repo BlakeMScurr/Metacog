246895d109bed913c4df1aa2fe692a00c9e9692e
blakemscurr <blakemscurr@gmail.com>
2021-06-08 15:03:48 +1200

    Make index page.

--- src/routes/index.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import Selector from "../components/Selector.svelte";
-
-    function onSelect(event) {
-        console.log(event.detail.choices)
-    }
-</script>
-
-<Selector on:select={onSelect}></Selector>
