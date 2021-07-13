72ec111e196e45809af9d810181ce3e6f29f5136
blakemscurr <blakemscurr@gmail.com>
2021-07-13 16:20:38 +1200

    Remove unused library files.

--- src/server/proposal.ts
+++
@@ -1,9 +0,0 @@
-import { action, signedAction } from "$lib/eth";
-
-export type channelProposal = makeRoom | joinRoom
-
-export class makeRoom extends action {
-    constructor() {
-        super("makeRoom")
-    }
-}
